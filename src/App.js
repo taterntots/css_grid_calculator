09ebac8b30933dc85f4a74b2bc17116f1257899d
taterntots <matthew.a.weidner@gmail.com>
2020-08-31 16:13:18 -0400

    styled the calculator using CSS grid. Not functional at the moment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div className="calculator-top">
+        0
+      </div>
+      <div class="calculator-body">
+        <button class="button clear-button">C</button>
+        <button class="button">÷</button>
+        <button class="button">7</button>
+        <button class="button">8</button>
+        <button class="button">9</button>
+        <button class="button">x</button>
+        <button class="button">4</button>
+        <button class="button">5</button>
+        <button class="button">6</button>
+        <button class="button">-</button>
+        <button class="button">1</button>
+        <button class="button">2</button>
+        <button class="button">3</button>
+        <button class="button">+</button>
+        <button class="button zero-button">0</button>
+        <button class="button equals-button">=</button>
+      </div>
+    </header>
   );
 }
 
